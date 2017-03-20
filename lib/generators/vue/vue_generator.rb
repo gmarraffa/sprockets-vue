820f67372ffe3a77d1ea8931e98eb1fe11b41e3b
maztek <marraffa3ai@gmail.com>
2017-03-20 10:20:54 +0100

    create generator

---
+++ lib/generators/vue/vue_generator.rb
@@ -0,0 +1,9 @@
+require 'rails/generators/base'
+class VueGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+  argument :lang, type: :string, default: 'coffee'
+
+  def generate_vue
+    template 'tpl.vue.erb', "app/assets/javascripts/#{file_path}.vue"
+  end
+end
