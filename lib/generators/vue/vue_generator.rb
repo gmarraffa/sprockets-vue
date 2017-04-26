6d0283cde2795fc99aac46fe45dca11b35e8e387
gmarraffa <marraffa3ai@gmail.com>
2017-04-26 12:05:04 +0200

    Changed default language

--- lib/generators/vue/vue_generator.rb
+++ lib/generators/vue/vue_generator.rb
@@ -1,7 +1,7 @@
 require 'rails/generators/base'
 class VueGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
-  argument :lang, type: :string, default: 'coffee'
+  argument :lang, type: :string, default: 'js'
 
   def generate_vue
     template 'tpl.vue.erb', "app/assets/javascripts/#{file_path}.vue"
